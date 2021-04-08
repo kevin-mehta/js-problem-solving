7a56765f47c43f8b33255bfbc3cdaa9d8af1ecf0
kevin-mehta <kevin.h.mehta@gmail.com>
2021-04-08 12:09:50 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-problem-solving
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-problem-solving)
---
+++ script.js
@@ -0,0 +1,48 @@
+/**
+ * Reverse the Case of all String characters
+ *
+ * https://edabit.com/challenge/PTvYKkvf7oytHmJFm
+ *
+ * Problem Statement:
+ * Given a string, create a function to reverse the case.
+ * All lower-cased letters should be upper-cased, and vice versa.
+ *
+ * Examples:
+ * reverseCase("Happy Birthday") ➞ "hAPPY bIRTHDAY"
+ * reverseCase("MANY THANKS") ➞ "many thanks"
+ * reverseCase("sPoNtAnEoUs") ➞ "SpOnTaNeOuS"
+ *
+ * Notes:
+ * NA
+ *
+ * Level: Easy
+ */
+
+/**
+ * Final Approach - Kevin Mehta
+ *
+ * @param str String whose characters cases needs to be reversed.
+ */
+const reverseCaseK = str => {
+  return str
+    .split("")
+    .map(value => {
+      if (/[A-Z]/.test(value)) {
+        return value.toLowerCase();
+      } else if (/[a-z]/.test(value)) {
+        return value.toUpperCase();
+      } else {
+        return value;
+      }
+    })
+    .join("");
+};
+/**
+ * Recommended Approach
+ *
+ * @param str String whose characters cases needs to be reversed.
+ */
+const reverseCase = str =>
+  [...str]
+    .map(c => (c.toLowerCase() == c ? c.toUpperCase() : c.toLowerCase()))
+    .join("");
