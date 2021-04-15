70f03b95e920f5c732ce95eaa0c6bff6bffcfe15
kevin-mehta <kevin.h.mehta@gmail.com>
2021-04-15 21:19:41 +0530

    2 files modified

--- data-structures/square-root-of-a-number.js
+++ data-structures/square-root-of-a-number.js
@@ -10,8 +10,12 @@
  *  d) If x is greater, do binary search between mid+1 and end.
  *  In this case, we also update ans (Note that we need floor).
  *  e) If x is smaller, do binary search between start and mid.
+ *
+ * https://stackoverflow.com/questions/12815651/binary-search-for-square-root-falls-into-infinite-loop-in-javascript
+ *
+ * https://www.tutorialspoint.com/find-square-root-of-number-upto-given-precision-using-binary-search-in-cplusplus
  */
-const floorSqrt = x => {
+const floorSqrt1 = x => {
   console.log("x: ", x);
   let i;
   // for (i = 0; Math.floor(i * i) <= x; i = i + 0.01) {
@@ -20,4 +24,41 @@ const floorSqrt = x => {
   //   }
   // }
   return Math.floor(i);
+};
+const floorSqrtK = x => {
+  let start = 0;
+  let end = x;
+  let numSqrt = getSqrtOfNumber(start, end);
+  return numSqrt;
+};
+const getSqrtOfNumber = (start, end) => {
+  if (start <= end) {
+    let mid = (start + end) / 2;
+    let midSquare = mid * mid;
+    if (end === midSquare) {
+      return end;
+    } else if (midSquare < end) {
+      getSqrtOfNumber(mid + 1, end);
+    } else {
+      getSqrtOfNumber(start, mid);
+    }
+  }
+};
+const performSearch = (start, end) => {
+  for (let i = start; i <= end; i++) {
+    let mid = (start + end) / 2;
+    if (x === mid * mid) {
+      return x;
+    } else if (x > mid * mid) {
+      performSearch(mid + 1, end);
+    } else {
+      performSearch(start, mid + 1);
+    }
+  }
+};
+
+const sqrtBinarySearchK = (num, precision) => {
+  let left = 0,
+    right = num;
+  let mid, res;
 };
