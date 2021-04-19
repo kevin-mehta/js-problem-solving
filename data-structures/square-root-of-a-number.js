eaaa3e7524edd4fd4fdf9d67d21c50a92121d27e
kevin-mehta <kevin.h.mehta@gmail.com>
2021-04-19 08:54:18 +0530

    Find the square root of a number.

--- data-structures/square-root-of-a-number.js
+++ data-structures/square-root-of-a-number.js
@@ -4,61 +4,53 @@
  * Approach:
  * 1) Start with ‘start’ = 0, end = ‘x’.
  * 2) Do following while ‘start’ is smaller than or equal to ‘end’.
- *  a) Compute ‘mid’ as (start + end)/2
- *  b) compare mid*mid with x.
- *  c) If x is equal to mid*mid, return mid.
- *  d) If x is greater, do binary search between mid+1 and end.
- *  In this case, we also update ans (Note that we need floor).
- *  e) If x is smaller, do binary search between start and mid.
+ *    a) Compute ‘mid’ as (start + end)/2
+ *    b) compare mid*mid with x.
+ *    c) If x is equal to mid*mid, return mid.
+ *    d) If x is greater, do binary search between mid+1 and end.
+ *    In this case, we also update ans (Note that we need floor).
+ *    e) If x is smaller, do binary search between start and mid.
+ * 
+ * Problem Statement:
+ * Given an integer x, find the square root of x. 
+ * If x is not a perfect square, then return floor(√x).
+ * 
+ * Examples:
+ * floorSqrtK(4) ➞ 2
+ * Since, 4 is a perfect square, so its square root is 2.
+ * floorSqrtK(5) ➞ 2
+ * Since, 5 is not a perfect square, floor of square root of 5 is 2.
+ * floorSqrtK(24648) ➞ 156
+ * 
+ * Notes:
+ * Try Solving the question without using sqrt Function.
+ * 
+ * Expected Time Complexity: O(log N)
+ * Expected Auxiliary Space: O(1)
  *
- * https://stackoverflow.com/questions/12815651/binary-search-for-square-root-falls-into-infinite-loop-in-javascript
+ * References:
+https://stackoverflow.com/questions/12815651/binary-search-for-square-root-falls-into-infinite-loop-in-javascript
  *
- * https://www.tutorialspoint.com/find-square-root-of-number-upto-given-precision-using-binary-search-in-cplusplus
+https://www.tutorialspoint.com/find-square-root-of-number-upto-given-precision-using-binary-search-in-cplusplus
+ *
  */
-const floorSqrt1 = x => {
-  console.log("x: ", x);
-  let i;
-  // for (i = 0; Math.floor(i * i) <= x; i = i + 0.01) {
-  //   if (Math.floor(i * i) === x) {
-  //     return Math.floor(i);
-  //   }
-  // }
-  return Math.floor(i);
-};
+/**
+ * Find the square root of a number.
+ *
+ * Recommended Approach
+ *
+ * @param x Number whose square root needs to be calculated.
+ */
 const floorSqrtK = x => {
-  let start = 0;
-  let end = x;
-  let numSqrt = getSqrtOfNumber(start, end);
-  return numSqrt;
-};
-const getSqrtOfNumber = (start, end) => {
-  if (start <= end) {
-    let mid = (start + end) / 2;
-    let midSquare = mid * mid;
-    if (end === midSquare) {
-      return end;
-    } else if (midSquare < end) {
-      getSqrtOfNumber(mid + 1, end);
+  var lo = 0,
+    hi = x;
+  while (lo <= hi) {
+    var mid = Math.floor((lo + hi) / 2);
+    if (mid * mid > x) {
+      hi = mid - 1;
     } else {
-      getSqrtOfNumber(start, mid);
+      lo = mid + 1;
     }
   }
-};
-const performSearch = (start, end) => {
-  for (let i = start; i <= end; i++) {
-    let mid = (start + end) / 2;
-    if (x === mid * mid) {
-      return x;
-    } else if (x > mid * mid) {
-      performSearch(mid + 1, end);
-    } else {
-      performSearch(start, mid + 1);
-    }
-  }
-};
-
-const sqrtBinarySearchK = (num, precision) => {
-  let left = 0,
-    right = num;
-  let mid, res;
+  return hi;
 };
