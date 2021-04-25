c4ca03cb1e8d0a3d851fac4f52ee7abe21181c95
kevin-mehta <kevin.h.mehta@gmail.com>
2021-04-25 15:10:33 +0530

    Police and Thieves.

---
+++ algo/police-and-thieves-1.js
@@ -0,0 +1,8 @@
+// let itr = 0;
+// let count = 0;
+const catchThieves1 = (arr, n, k) => {
+  traverseArray(arr, n, k, itr);
+  return count;
+};
+
+
--- algo/police-and-thieves.js
+++ algo/police-and-thieves.js
@@ -1,3 +1,35 @@
-const catchThieves = (arr, n, k) => {
-  return false;
-}
+let itr = 0;
+let count = 0;
+const catchThievesK1 = (arr, n, k) => {
+  traverseArray(arr, n, k, itr);
+  return count;
+};
+const traverseArray = (arr, n, k, itr) => {
+  if (itr <= n - 1) {
+    if (arr[itr] === "P" && itr - k >= 0 && itr - k <= n) {
+      let tempSlicedArr = arr.slice(itr - k, itr);
+      tempSlicedArr.includes("T") && count++;
+    } else if (arr[itr] === "P" && itr + k >= 0 && itr - k <= n) {
+      let tempSlicedArr = arr.slice(k, itr + k + 1);
+      tempSlicedArr.includes("T") && count++;
+    }
+    itr++;
+    traverseArray(arr, n, k, itr);
+  }
+};
+
+const catchThievesK = (arr, n, k) => {
+  let itr = 0;
+  let count = 0;
+  while (itr <= n - 1) {
+    if (arr[itr] === "P" && itr - k >= 0 && itr - k <= n) {
+      let tempSlicedArr = arr.slice(itr - k, itr);
+      tempSlicedArr.includes("T") && count++;
+    } else if (arr[itr] === "P" && itr + k >= 0 && itr - k <= n) {
+      let tempSlicedArr = arr.slice(k, itr + k + 1);
+      tempSlicedArr.includes("T") && count++;
+    }
+    itr++;
+  }
+  return count;
+};
