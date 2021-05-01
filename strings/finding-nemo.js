cc5f5e34e2a226c8a8bcc05aecd93ffeb53e15a8
kevin-mehta <kevin.h.mehta@gmail.com>
2021-05-01 12:55:09 +0530

    Finding Nemo

--- strings/finding-nemo.js
+++ strings/finding-nemo.js
@@ -5,8 +5,8 @@
  *
  * Problem Statement:
  * You're given a string of words.
- * You need to find the word "Nemo", and return a 
- * string like this: 
+ * You need to find the word "Nemo", and return a
+ * string like this:
  * "I found Nemo at [the order of the word you find nemo]!".
  *
  * If you can't find Nemo, return "I can't find Nemo :(".
@@ -18,11 +18,14 @@
  *
  * Notes:
  * ! , ? . are always separated from the last word.
- * Nemo will always look like Nemo, and not NeMo or 
+ *
+ * Nemo will always look like Nemo, and not NeMo or
  * other capital variations.
- * Nemo's, or anything that says Nemo with something 
+ *
+ * Nemo's, or anything that says Nemo with something
  * behind it, doesn't count as Finding Nemo.
- * If there are multiple Nemo's in the sentence, 
+ *
+ * If there are multiple Nemo's in the sentence,
  * only return for the first one.
  *
  * Level: Easy
