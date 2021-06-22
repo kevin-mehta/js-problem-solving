93d4f714950f4edd270aa377deb3cc6c31f32655
kevin-mehta <kevin.h.mehta@gmail.com>
2021-06-22 19:56:22 +0530

    Alphabet Soup

---
+++ strings/alphabet-soup.js
@@ -0,0 +1,43 @@
+/**
+ * Alphabet Soup
+ *
+ * https://edabit.com/challenge/bHDbMWpdeNaw23jqT
+ *
+ * Problem Statement:
+ * Create a function that takes a string and returns a string
+ * with its letters in alphabetical order.
+ *
+ * Examples:
+ * alphabetSoup("hello") ➞ "ehllo"
+ * alphabetSoup("edabit") ➞ "abdeit"
+ * alphabetSoup("hacker") ➞ "acehkr"
+ * alphabetSoup("geek") ➞ "eegk"
+ * alphabetSoup("javascript") ➞ "aacijprstv"
+ *
+ * Notes:
+ * You can assume numbers and punctuation symbols won't
+ * be included in test cases. You'll only have to deal
+ * with single word, alphabetic characters.
+ *
+ * Level: Easy
+ */
+
+/**
+ * Final Approach - Kevin Mehta
+ *
+ * @param str String whose letters needs to be arranged
+ * in an alphabetical order.
+ */
+const alphabetSoupK = str =>
+  str
+    .split('')
+    .sort()
+    .join('');
+
+/**
+ * Recommended Approach.
+ *
+ * @param str String whose letters needs to be arranged
+ * in an alphabetical order.
+ */
+const alphabetSoup = str => [...str].sort().join('');
