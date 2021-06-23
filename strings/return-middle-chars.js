0fb7ce0da8a3b181083e2752f4a95349b70c8741
kevin-mehta <kevin.h.mehta@gmail.com>
2021-06-23 08:20:26 +0530

    Return the Middle Character(s) of a String. 1 file modified.

--- strings/return-middle-chars.js
+++ strings/return-middle-chars.js
@@ -1,3 +1,33 @@
+/**
+ * Return the Middle Character(s) of a String
+ *
+ * https://edabit.com/challenge/R3649PDvoFf6wGxyg
+ *
+ * Problem Statement:
+ * Create a function that takes a string and returns the
+ * middle character(s). If the word's length is odd,
+ * return the middle character.
+ * If the word's length is even, return the middle two
+ * characters.
+ *
+ * Examples:
+ * getMiddle("test") ➞ "es"
+ * getMiddle("testing") ➞ "t"
+ * getMiddle("middle") ➞ "dd"
+ * getMiddle("A") ➞ "A"
+ *
+ * Notes:
+ * All test cases contain a single word (as a string).
+ *
+ * Level: Easy
+ */
+
+/**
+ * Final Approach - Kevin Mehta
+ *
+ * @param str String whose middle characters needs to be
+ * found.
+ */
 const getMiddleK = str => {
   let slen = str.length;
   let midd = slen / 2;
@@ -11,6 +41,12 @@ const getMiddleK = str => {
   return pluckedChars.join('');
 };
 
+/**
+ * Recommended Approach
+ *
+ * @param str String whose middle characters needs to be
+ * found.
+ */
 const getMiddle = str => {
   let start = Math.ceil(str.length / 2) - 1;
   let end = Math.floor(str.length / 2) + 1;
