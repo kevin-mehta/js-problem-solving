d33e055c4f6a9926fce1743cb605cf46fea431f9
kevin-mehta <kevin.h.mehta@gmail.com>
2021-06-29 20:58:24 +0530

    Letters Only. 1 file modified.

--- strings/letters-only.js
+++ strings/letters-only.js
@@ -11,6 +11,20 @@
  * lettersOnly("R!=:~0o0./c&}9k`60=y") ➞ "Rocky"
  * lettersOnly("^,]%4B|@56a![0{2m>b1&4i4") ➞ "Bambi"
  * lettersOnly("^U)6$22>8p).") ➞ "Up"
- * 
+ *
  * Level: Easy
  */
+
+/**
+ * Final Approach - Kevin Mehta
+ *
+ * @param str String whose letters needs to be fetched.
+ */
+const lettersOnly = str => str.replace(/[^A-Za-z]/g, '');
+
+/**
+ * Recommended Approach
+ *
+ * @param str String whose letters needs to be fetched.
+ */
+const lettersOnly = str => str.replace(/[^a-z]/gi, '');
