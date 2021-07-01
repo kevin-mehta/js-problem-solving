3680a1e6f670b557bbe641724621032b98ea9758
kevin-mehta <kevin.h.mehta@gmail.com>
2021-07-01 23:12:02 +0530

    Shared Letters. 1 file modified.

--- strings/shared-letters.js
+++ strings/shared-letters.js
@@ -1,15 +1,10 @@
 const sharedLettersK = (str1, str2) => {
-  // let arr1 = [...str1];
-  // console.log(arr1);
   let arr2 = [...str2];
   let count = 0;
-  for(let a of str1) {
-    // console.log(a);
-    // console.log(arr2);
-
-    if(arr2.includes(a)) {
+  for (let a of str1) {
+    if (arr2.includes(a)) {
       count++;
     }
   }
   return count;
-}
+};
