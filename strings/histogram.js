bde54a60c749eb769a1e65853f0589141f305c7b
kevin-mehta <kevin.h.mehta@gmail.com>
2021-07-02 23:44:15 +0530

    Histogram. 1 file modified.

--- strings/histogram.js
+++ strings/histogram.js
@@ -1,7 +1,45 @@
+/**
+ * Histogram Function
+ *
+ * https://edabit.com/challenge/st8SZGoa3WE8Xrhf7
+ *
+ * Problem Statement:
+ * Build a function that creates histograms.
+ * Every bar needs to be on a new line and its length
+ * corresponds to the numbers in the array passed as an
+ * argument. The second argument of the function represents
+ * the character to be used for the bar.
+ * histogram(arr, char) ➞ str
+ *
+ * Examples:
+ * histogram([1, 3, 4], "#") ➞ "#\n###\n####"
+ * #
+ * ###
+ * ####
+ *
+ * histogram([6, 2, 15, 3], "=") ➞ "======\n==\n===============\n==="
+ * ======
+ * ==
+ * ===============
+ * ===
+ *
+ * histogram([1, 10], "+") ➞ "+\n++++++++++"
+ * +
+ * ++++++++++
+ *
+ * Level: Medium
+ */
+
+/**
+ * Final Approach - Kevin Mehta
+ *
+ * @param arr
+ * @param char
+ */
 const histogramK = (arr, char) => {
-  let strHistogram = "";
+  let strHistogram = '';
   for (let val of arr) {
-    strHistogram += char.repeat(val) + "\n";
+    strHistogram += char.repeat(val) + '\n';
   }
   return strHistogram;
 };
