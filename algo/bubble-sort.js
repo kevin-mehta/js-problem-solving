cd33b612bf55b89db7c47be8adb6fb3b93b30904
kevin-mehta <kevin.h.mehta@gmail.com>
2021-08-14 11:44:01 +0530

    2 files modified

--- algo/bubble-sort.js
+++ algo/bubble-sort.js
@@ -1,1 +1,19 @@
-const bubbleSort = arr => {};
+const sort = arr => {
+  let len = arr.length;
+  bubbleSort(arr, len);
+  return arr;
+};
+const bubbleSort = (arr, n) => {
+  for (let i = 0; i < n - 1; i++) {
+    for (let j = 0; j < n - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        swap(arr, j, j + 1);
+      }
+    }
+  }
+};
+const swap = (arr, xp, yp) => {
+  let temp = arr[xp];
+  arr[xp] = arr[yp];
+  arr[yp] = temp;
+};
