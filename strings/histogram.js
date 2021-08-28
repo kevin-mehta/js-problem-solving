d49e8a8714102e561dfd4c5c8ffe51229f84360c
kevin-mehta <kevin.h.mehta@gmail.com>
2021-08-28 11:11:09 +0530

    1 file modified

--- strings/histogram.js
+++ strings/histogram.js
@@ -7,7 +7,7 @@
  * Build a function that creates histograms.
  * Every bar needs to be on a new line and its length
  * corresponds to the numbers in the array passed as an
- * argument. The second argument of the function represents
+ * argument. The second argument of the function
  * the character to be used for the bar.
  * histogram(arr, char) ➞ str
  *
