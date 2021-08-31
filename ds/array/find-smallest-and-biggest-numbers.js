4c13f3a1ff99fd75f9200aedbfebb24e859c782e
kevin-mehta <kevin.h.mehta@gmail.com>
2021-08-31 22:29:17 +0530

    1 file modified

--- ds/array/find-smallest-and-biggest-numbers.js
+++ ds/array/find-smallest-and-biggest-numbers.js
@@ -4,7 +4,7 @@
  *
  * Problem Statement:
  * Create a function that takes an array of numbers and
- * return both the minimum and maximum numbers, in that .
+ * return both the minimum and maximum numbers, in that order.
  *
  * Examples:
  * minMax([1, 2, 3, 4, 5]) ➞ [1, 5]
