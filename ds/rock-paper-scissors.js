2974db787a3495665a6126b52cb63c5edef7d5a2
kevin-mehta <kevin.h.mehta@gmail.com>
2021-09-20 17:13:26 +0530

    1 file modified

--- ds/rock-paper-scissors.js
+++ ds/rock-paper-scissors.js
@@ -1,19 +1,19 @@
 const rpsK = (p1, p2) => {
   return p1 === p2
     ? "It's a draw"
-    : (p1 === "Rock" && p2 === "Paper") ||
-      (p1 === "Paper" && p2 === "Scissors") ||
-      (p1 === "Scissors" && p2 === "Rock")
-    ? "The winner is p2"
-    : "The winner is p1";
+    : (p1 === 'Rock' && p2 === 'Paper') ||
+      (p1 === 'Paper' && p2 === 'Scissors') ||
+      (p1 === 'Scissors' && p2 === 'Rock')
+    ? 'The winner is p2'
+    : 'The winner is p1';
 };
 
 const rps = (p1, p2) => {
   let w = {
-    Rock: "Scissors",
-    Scissors: "Paper",
-    Paper: "Rock"
+    Rock: 'Scissors',
+    Scissors: 'Paper',
+    Paper: 'Rock',
   };
-  if (p1 === p2) return "It's a draw";
-  return `The winner is ${w[p1] === p2 ? "p1" : "p2"}`;
+  if (p1 === p2) return "It's a ";
+  return `The winner is ${w[p1] === p2 ? 'p1' : 'p2'}`;
 };
