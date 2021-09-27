ce4f256385bf1e69e3af237460be834eb5b2e0e0
kevin-mehta <kevin.h.mehta@gmail.com>
2021-09-27 13:19:38 +0530

    1 file modified or "Get JS object changes".

--- misc/get-object-changes.js
+++ misc/get-object-changes.js
@@ -8,6 +8,9 @@ let objChanged = [
   { name: 'test02', age: 20 },
   { name: 'test03', age: 20 },
 ];
+/**
+ * Identify whether a JS object has changed.
+ */
 getObjChanges = () => {
   let changes, i, j, len, item;
   if (JSON.stringify(objOriginal) === JSON.stringify(objChanged)) {
