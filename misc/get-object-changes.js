36f77b35e7dce0a66b87580db5d2d5b29faf4b15
kevin-mehta <kevin.h.mehta@gmail.com>
2021-09-27 13:20:10 +0530

    1 file modified or "Get JS object changes".

--- misc/get-object-changes.js
+++ misc/get-object-changes.js
@@ -1,3 +1,26 @@
+/**
+ * Alphabet Soup
+ *
+ * https://edabit.com/challenge/bHDbMWpdeNaw23jqT
+ *
+ * Problem Statement:
+ * Create a function that takes a string and returns a string
+ * with its letters in alphabetical order.
+ *
+ * Examples:
+ * alphabetSoup("hello") ➞ "ehllo"
+ * alphabetSoup("edabit") ➞ "abdeit"
+ * alphabetSoup("hacker") ➞ "acehkr"
+ * alphabetSoup("geek") ➞ "eegk"
+ * alphabetSoup("javascript") ➞ "aacijprstv"
+ *
+ * Notes:
+ * You can assume numbers and punctuation symbols won't
+ * be included in test cases. You'll only have to deal
+ * with single word, alphabetic characters.
+ *
+ * Level: Easy
+ */
 let objOriginal = [
   { name: 'test01', age: 10 },
   { name: 'test02', age: 20 },
