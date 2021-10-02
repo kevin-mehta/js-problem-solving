7469fc26e4579a2106d8215bae07cba212a0dce9
kevin-mehta <kevin.h.mehta@gmail.com>
2021-10-02 13:01:48 +0530

    2 files modified for "Get JS Object changes".

---
+++ misc/get-object-changes.js
@@ -0,0 +1,15 @@
+let objOriginal = [
+  { name: 'test01', age: 10 },
+  { name: 'test02', age: 20 },
+  { name: 'test03', age: 30 },
+];
+let objChanged = [
+  { name: 'test01', age: 10 },
+  { name: 'test02', age: 20 },
+  { name: 'test03', age: 20 },
+];
+getObjChanges = () => {
+  let changes = [];
+  
+  return changes;
+};
