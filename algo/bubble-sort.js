dbbe702130e0ec1d1ceaea042abfe45d9e498eba
kevin-mehta <kevin.h.mehta@gmail.com>
2021-10-05 13:56:05 +0530

    1 file modified

--- algo/bubble-sort.js
+++ algo/bubble-sort.js
@@ -3,7 +3,7 @@ const sort = (arr) => {
   bubbleSort(arr, len);
   return arr;
 };
-const  = (arr, n) => {
+const bubbleSort = (arr, n) => {
   for (let i = 0; i < n - 1; i++) {
     for (let j = 0; j < n - i - 1; j++) {
       if (arr[j] > arr[j + 1]) {
