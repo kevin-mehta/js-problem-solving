66ea9514f644023da12c1bccec83a59d51899cbe
kevin-mehta <kevin.h.mehta@gmail.com>
2021-11-18 10:22:57 +0530

    1 file modified

--- strings/return-middle-chars.js
+++ strings/return-middle-chars.js
@@ -6,7 +6,7 @@
  * Problem Statement:
  * Create a function that takes a string and returns the
  * middle character(s). If the word's length is odd,
- * return the middle .
+ * return the middle character.
  * If the word's length is even, return the middle two
  * characters.
  *
