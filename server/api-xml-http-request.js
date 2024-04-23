6f127c68785d3ad863aa8851ab42bfae50d75622
kevin-mehta <kevin.h.mehta@gmail.com>
2024-04-23 20:00:25 +0530

    2 files modified

--- server/api-xml-http-request.js
+++ server/api-xml-http-request.js
@@ -1,3 +1,14 @@
 function invokeXmlHttpRequest() {
-  let xhr = new XMLHttpRequest();
+  let xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+      console.log(xhttp.responseText);
+    }
+  };
+  xhttp.open(
+    'GET',
+    'https://jsonplaceholder.typicode.com/users',
+    true //async
+  );
+  xhttp.send();
 }
