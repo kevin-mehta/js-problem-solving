d9ed72e50ccbdc6761edb295fe62ae4947a99482
kevin-mehta <kevin.h.mehta@gmail.com>
2024-05-26 13:36:10 +0530

    For 'comment-box', tried appending content on button click.

--- components/comment-box/comment-box.js
+++ components/comment-box/comment-box.js
@@ -4,5 +4,18 @@ document.addEventListener('DOMContentLoaded', (event) => {
   let eleReplyButton = document.getElementById('btnReply');
   eleReplyButton.addEventListener('click', (event) => {
     console.log('Reply button clicked.');
+
+    document.getElementById('postBox').innerHTML += `
+    <div class="comment-box"><input class="comment-box__name" type="text" placeholder="Your name" />
+	<textarea
+	  class="comment-box__desc"
+	  cols="30"
+	  rows="3"
+	  placeholder="comment"
+	></textarea>
+	<button class="comment-box__post">Post</button>
+	<button class="comment-box__cancel">Cancel</button>
+</div>
+    `;
   });
 });
