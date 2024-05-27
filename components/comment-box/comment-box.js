4dd476e5a2cd100a2ab0fdecfb8625b6253e1cf0
kevin-mehta <kevin.h.mehta@gmail.com>
2024-05-27 13:25:23 +0530

    For 'comment-box',  modified CSS class names.

--- components/comment-box/comment-box.js
+++ components/comment-box/comment-box.js
@@ -2,5 +2,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
   console.log('DOM loaded.');
 
   let eleReplyButton = document.getElementById('btnReply');
-  eleReplyButton.addEventListener('click', (event) => {});
+  eleReplyButton.addEventListener('click', (event) => {
+    console.log('Reply button clicked.');
+  });
 });
