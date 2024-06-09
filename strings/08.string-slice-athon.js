5614834fd74e71cb8938ee4e011d9b37f0f288bd
kevin-mehta <kevin.h.mehta@gmail.com>
2024-06-09 12:10:51 +0530

    For the 'function-function', implemented a optimized solution 1 'first6CharsInReverse'.

--- strings/08.string-slice-athon.js
+++ strings/08.string-slice-athon.js
@@ -1,19 +1,37 @@
 function first5CharsK1(s) {
-	return s.slice(0, 5)
+  return s.slice(0, 5);
 }
 function last5CharsK1(s) {
-	return s.slice(-5);
+  return s.slice(-5);
 }
 function allCharsFromBackK1(s) {
-	return s.split("").reverse().join("");
+  return s.split('').reverse().join('');
 }
 function first6CharsInReverseK1(s) {
-	return s.split("").reverse().join("").slice(s.length-6);
+  return s
+    .split('')
+    .reverse()
+    .join('')
+    .slice(s.length - 6);
 }
 function last7CharsOddPosK1(s) {
-	return s.split("").reverse().join("").slice(0, 7).split("").map((v, i, arr) => i%2 === 0 ? v : undefined).reverse().join("");
+  return s
+    .split('')
+    .reverse()
+    .join('')
+    .slice(0, 7)
+    .split('')
+    .map((v, i, arr) => (i % 2 === 0 ? v : undefined))
+    .reverse()
+    .join('');
 }
 
-const first5CharsK2 = s => s.slice(0, 5);
-const last5CharsK2 = s => s.slice(-5);
-const allCharsFromBackK2 = s => s.split("").reverse().join("");
+const first5CharsK2 = (s) => s.slice(0, 5);
+const last5CharsK2 = (s) => s.slice(-5);
+const allCharsFromBackK2 = (s) => s.split('').reverse().join('');
+const first6CharsInReverseK2 = (s) =>
+  s
+    .split('')
+    .reverse()
+    .join('')
+    .slice(s.length - 6);
