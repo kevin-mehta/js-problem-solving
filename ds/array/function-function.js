0cc9e80abe5b6a83d6916966afeff49678a4bd9c
kevin-mehta <kevin.h.mehta@gmail.com>
2024-06-14 13:04:47 +0530

    For the 'function-function', added a URL, tags, and a problem statement.

--- ds/array/function-function.js
+++ ds/array/function-function.js
@@ -1,3 +1,18 @@
+/**
+ * Function Function
+ * Published by BijogFc24 in JavaScript.
+ * 
+ * URL:
+ * https://edabit.com/challenge/FEiLd6HgHcwkgHTWP
+ * 
+ * Tags:
+ * arrays, closures, functional_programming
+ * 
+ * Problem Statement:
+ * Create a function that takes a number as its parameter and returns another function. 
+ * The returned function must take an array of numbers as its parameter, and return an 
+ * array of the numbers divided by the number that was passed into the first function.
+ */
 function factory1(num) {
   return function (arrDividends) {
     return arrDividends.map(function (value, index, array) {
