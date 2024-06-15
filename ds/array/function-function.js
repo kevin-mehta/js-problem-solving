4c510deb66ff1872d38e40e02e0eb4fa7023bf12
kevin-mehta <kevin.h.mehta@gmail.com>
2024-06-15 12:51:16 +0530

    For the 'function-function', implemented a optimized solution 1.

--- ds/array/function-function.js
+++ ds/array/function-function.js
@@ -1,7 +1,9 @@
-function factory(num) {
+function factory1(num) {
   return function (arrDividends) {
     return arrDividends.map(function (value, index, array) {
       return value / num;
     });
   };
 }
+
+const factory = (n) => (arr) => arr.map((v) => v / n);
